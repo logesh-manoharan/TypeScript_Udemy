b4fc319826153394d84f5bcf3f2fc29560a545a2
Logesh Manoharan <logesh_manoharan@comcast.com>
2021-11-30 19:02:22 +0530

    All_the_RESOURCES

--- Practices/project1/script.js
+++ Practices/project1/script.js
@@ -43,3 +43,17 @@ var Days;
     Days[Days["WEDNESDAY"] = 2] = "WEDNESDAY";
 })(Days || (Days = {}));
 console.log(Days[Days.MONDAY] + " " + Days[Days.TUESDAY] + " " + Days[Days.WEDNESDAY]);
+// UNION  (by make use of '|' symbol)
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var added = combine(3, 5);
+var combined = combine("Logesh", "Mano");
+console.log("Integer Addition : " + added + " String Combined : " + combined);
--- Practices/project1/script.ts
+++ Practices/project1/script.ts
@@ -82,5 +82,30 @@ enum Days {
 console.log(Days[Days.MONDAY] + " " + Days[Days.TUESDAY] + " " + Days[Days.WEDNESDAY]);
 
 
+// UNION  (by make use of '|' symbol)
+
+function combine (input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+
+
+const added = combine(3, 5);
+const combined = combine("Logesh", "Mano"); 
+
+console.log("Integer Addition : " + added + " String Combined : " + combined);
+
+
+
+
+// LITERAL
+
+
 
 
