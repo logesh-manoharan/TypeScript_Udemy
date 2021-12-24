9fe5d918222ee99daa5d26f5b3e88b9071d54282
logesh-manoharan <logeshmano189@gmail.com>
2021-12-24 22:26:34 +0530

    DragAndDrop till lecturer 128

--- Practices/DragAndDrop/src/app.ts
+++ Practices/DragAndDrop/src/app.ts
@@ -47,6 +47,95 @@ function Autobind (_target: any, _methodName: any, descriptor: PropertyDescripto
     return adjDescriptor;
 }
 
+
+// Creating PROJECCT STATE using 'class' [STATE should be ONLY ONE. So, we are creating the SINGLETON class]
+class ProjectState {
+    listeners: any[];
+    projects: any[];
+    private static instance: ProjectState;
+
+    constructor () {
+        this.listeners = [];
+        this.projects = [];
+    }
+
+    static getInstance () {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new ProjectState();
+        return this.instance;
+    }
+
+    addListeners (listenerFunc: Function) {
+        this.listeners.push(listenerFunc);
+    }
+
+    addProjects (title: string, description: string, noOfPeoples: number) {
+        const newProject = {
+            id: Math.random().toString(),
+            title: title,
+            description: description,
+            peoples: noOfPeoples
+        };
+        this.projects.push(newProject);
+
+        for (const listenerFunc of this.listeners) {
+            listenerFunc(this.projects.slice());
+        }
+    }
+}
+
+const projectState = ProjectState.getInstance();
+
+// ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement; 
+    elementToInsert: HTMLElement;
+    assignedProjects: any[];
+
+    constructor (private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        this.assignedProjects = [];
+
+        const importNode = document.importNode(this.templateElement.content, true);
+
+        this.elementToInsert = importNode.firstElementChild as HTMLElement;
+        this.elementToInsert.id = `${this.type}-projects`;
+
+        projectState.addListeners((projects: any[]) => {
+            this.assignedProjects = projects;
+            this.renderProjects();
+        })
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderProjects () {
+        for (var i=0;i<this.assignedProjects.length;i++) {
+            var list = document.getElementById(`${this.type}-projects-list`)!;
+            var lstElement = document.createElement("li");
+            lstElement.textContent = this.assignedProjects[i]["title"];
+            list.appendChild(lstElement);
+        }
+    }
+
+    private renderContent () {
+        const listId = `${this.type}-projects-list`;
+        this.elementToInsert.querySelector('ul')!.id = listId;
+        this.elementToInsert.querySelector('h2')!.textContent = this.type.toUpperCase() + " ITEMS";
+    }
+
+    private attach () {
+        this.hostElement.insertAdjacentElement("beforeend", this.elementToInsert);
+    }
+} 
+
+//  ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -119,6 +208,9 @@ class ProjectInput {
             var [title, description, people] = userInput;
             console.log("Title : " + title + "" + "\nDescription : " + description 
             + "\nPeople : " + people);
+            
+            // adding the project into LIST [which is in Project STATEs]
+            projectState.addProjects(title, description, people);
         }
         this.clearInputs();
     }
@@ -134,4 +226,7 @@ class ProjectInput {
 
 
 const projInput = new ProjectInput();
+
+const activeProjList = new ProjectList('active');
+const finishedProjList = new ProjectList('finished');
 
